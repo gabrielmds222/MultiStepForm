adf4cf9e86c9122ad91f465b0a00e58acd107737
gabrielmds222 <gabrielmedsilva@outlook.com>
2023-01-19 02:13:51 -0300

    feat: Cria estrutura do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { ArrowLeft, ArrowRight } from "phosphor-react";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Formulário hello world</h1>
+    <div className="container">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="actions">
+            <button type="button">
+              <ArrowLeft size={32} color="#802323" weight="duotone" />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Continuar</span>
+              <ArrowRight size={32} color="#802323" weight="duotone" />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
